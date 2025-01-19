6e01886d943d3cbe4dbeb679101a3c5ed6f7ed7a
Benjamin Team Rocket <benjamin@team.rocket.com>
2025-01-19 18:50:10 -0300

    feat: replace code_scanner with summarize_sources command

--- dev/__main__.py
+++ dev/__main__.py
@@ -1,10 +1,10 @@
 import click
-from code_scanner import code_scanner
+from sumarize_source_code import summarize_sources
 
 @click.group(help='Herramientas terminal bash')
 def cli():
     pass
 
-cli.add_command(code_scanner)
+cli.add_command(summarize_sources)
 
 cli()
---
+++ dev/sumarize_source_code.py
@@ -0,0 +1,60 @@
+import os
+import click
+from pathlib import Path
+
+IGNORE_DIRECTORIES = ["node_modules", "venv", ".venv", "__pycache__"]
+
+
+def get_file_type(file_name):
+    return os.path.splitext(file_name)[1][1:] or "txt"
+
+
+def source_code_list(src_files):
+    content = []
+    for src in src_files:
+        content.append(f"- {src}")
+    return "\n".join(content)
+
+
+def source_code_content(src_files):
+    content = []
+    for src in src_files:
+        file_ext = get_file_type(src)
+        content_code = Path(src).read_text()
+        content.append(f"**{src}**\n\n```{file_ext}\n{content_code}\n```")
+    return "\n".join(content)
+
+
+def filter_source_code_files(src_files):
+    filtered = []
+    for file in src_files:
+        if not os.path.isfile(file):
+            continue
+        if any(directory in file for directory in IGNORE_DIRECTORIES):
+            continue
+        filtered.append(file)
+    return filtered
+
+
+def summary_source_code(src_files):
+    summary = []
+    source_list = source_code_list(src_files)
+    source_content = source_code_content(src_files)
+    summary.append("# Code Summary\n")
+    summary.append("## File list\n")
+    summary.append(source_list)
+    summary.append("\n ## File content")
+    summary.append(source_content)
+    return "\n".join(summary)
+
+
+@click.command(help="Resume codigo fuente en formato markdown")
+@click.option("-o", "--output", default=None, help="Output file in markdown format")
+@click.argument("src_files", nargs=-1)
+def summarize_sources(output, src_files):
+    src_files_filtered = filter_source_code_files(src_files)
+    summary = summary_source_code(src_files_filtered)
+    if output:
+        Path(f"{output}.md").write_text(summary)
+    else:
+        print(summary)
